550a7655d6d51818a30cad83011400274c055c80
Tyler Long <tyler4long@gmail.com>
2023-04-05 09:50:25 -0700

    Remove antd and manate

--- src/app.tsx
+++ src/app.tsx
@@ -1,36 +1,7 @@
 import React from 'react';
-import { Button, Space, Typography } from 'antd';
-import { auto } from 'manate/react';
 
-import { Store } from './store';
-
-const { Text, Title } = Typography;
-
-const App = (props: { store: Store }) => {
-  const { store } = props;
-  const render = () => (
-    <>
-      <Title>Untitled App</Title>
-      <Space>
-        <Button
-          onClick={() => {
-            store.count -= 1;
-          }}
-        >
-          -
-        </Button>
-        <Text>{store.count}</Text>
-        <Button
-          onClick={() => {
-            store.count += 1;
-          }}
-        >
-          +
-        </Button>
-      </Space>
-    </>
-  );
-  return auto(render, props);
+const App = () => {
+  return <div>Hello World</div>;
 };
 
 export default App;
