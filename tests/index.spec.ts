4a85a543955993a9482ca5b35ba159188bcfa10e
Tyler Long <tyler4long@gmail.com>
2023-04-05 15:26:05 -0700

    Add more test cases

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,6 +1,25 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('Home Page', async ({ page }) => {
   await page.goto('http://localhost:1234/');
-  await expect(page).toHaveTitle(/MacMate.app/);
+  await expect(page).toHaveTitle('MacMate.app');
+});
+
+test('Icon Builder Plus', async ({ page }) => {
+  await page.goto('http://localhost:1234/');
+  await page.getByText('Icon Builder Plus').click();
+  await expect(page).toHaveTitle('Icon Builder Plus - MacMate.app');
+});
+
+test('TypeScript Playground', async ({ page }) => {
+  await page.goto('http://localhost:1234/');
+  await page.getByText('TypeScript Playground').click();
+  await expect(page).toHaveTitle('TypeScript Playground - MacMate.app');
+});
+
+test('Page content', async ({ page }) => {
+  await page.goto('http://localhost:1234/');
+  await page.getByText('TypeScript Playground').click();
+  const content = await page.content();
+  expect(content).toContain('<title>TypeScript Playground - MacMate.app</title>');
 });
