c9ab9276f60c241122514c69510b8dace172fed7
Tyler Liu <tyler4long@gmail.com>
2023-04-06 19:49:26 -0700

    Gather assets from apps

---
+++ scripts/gather.ts
@@ -0,0 +1,32 @@
+import { run } from 'shell-commands';
+import { readdirSync, writeFileSync } from 'fs';
+
+import { apps } from '../src/constants';
+
+const gather = async () => {
+  // todo: enable this for all apps, remove slice
+  for (const a of apps.slice(0, 1)) {
+    const app = a.path.substring(1, a.path.length - 1);
+    await run(`
+      mkdir -p src/assets/${app}
+      cp -r ~/src/ts/${app}/icon.png src/assets/${app}/
+      mkdir -p src/assets/${app}/screenshots
+      cp -r ~/src/ts/${app}/test/screenshots/[0-9].png src/assets/${app}/screenshots/
+      mkdir -p src/assets/${app}/videos
+      cp -r ~/src/ts/${app}/test/videos/[0-9].mp4 src/assets/${app}/videos/
+    `);
+    const screenshotCounts = readdirSync(`src/assets/${app}/screenshots`).length;
+    const screenshotIndexes = Array.from({ length: screenshotCounts }, (_, i) => i);
+    const videoCounts = readdirSync(`src/assets/${app}/videos`).length / 2;
+    const videoIndexes = Array.from({ length: videoCounts }, (_, i) => i);
+    let code = screenshotIndexes.map((i) => `import screenshot${i} from './screenshots/${i}.png';\n`).join('');
+    code += videoIndexes.map((i) => `import video${i} from './videos/${i}.mp4';\n`).join('');
+    code += `
+export const screenshots = [${screenshotIndexes.map((i) => `screenshot${i}`).join(', ')}];
+export const videos = [${videoIndexes.map((i) => `video${i}`).join(', ')}];
+`;
+    writeFileSync(`src/assets/${app}/index.ts`, code);
+  }
+};
+
+gather();
--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -2,3 +2,15 @@ declare module '*.jpg' {
   const value: string;
   export default value;
 }
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
+declare module '*.mp4' {
+  const value: string;
+  export default value;
+}
+declare module '*.webm' {
+  const value: string;
+  export default value;
+}
