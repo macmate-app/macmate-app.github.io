c2436ca9452f79e44a20347f80921bbac49f9f3b
Tyler Liu <tyler4long@gmail.com>
2023-04-08 21:16:39 -0700

    Show download image

--- src/body/app.tsx
+++ src/body/app.tsx
@@ -3,10 +3,11 @@ import React from 'react';
 import Screenshots from './screenshots';
 import Vidoes from './videos';
 import { mdi } from '../utils';
+import macappstore from '../assets/macappstore.png';
 
 const App = (props: {
   assets: { screenshots: string[]; videos: string[]; icon: string };
-  marketing: { name: string; description: string; features: string };
+  marketing: { name: string; description: string; features: string; download: string | undefined };
 }) => {
   const { screenshots, videos, icon } = props.assets;
   const { marketing } = props;
@@ -18,6 +19,18 @@ const App = (props: {
       <Screenshots screenshots={screenshots} />
       <h2>Features</h2>
       <div dangerouslySetInnerHTML={{ __html: mdi.render(marketing.features) }}></div>
+      <h2>Download</h2>
+      {marketing.download && (
+        <a href="https://apps.apple.com/us/app/icon-builder-plus/id6447149673?mt=12" target="_blank">
+          <img src={macappstore} />
+        </a>
+      )}
+      {marketing.download === undefined && (
+        <>
+          <img src={macappstore} />
+          <p>This app will be listed in the Mac App Store soon, please stay tuned.</p>
+        </>
+      )}
       <h2>Video Demo</h2>
       <Vidoes videos={videos} />
     </div>
