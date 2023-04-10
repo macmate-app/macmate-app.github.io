2daa06f60381a5b503104727e329723e677d6368
Tyler Liu <tyler4long@gmail.com>
2023-04-09 17:25:48 -0700

    Rewrite tests

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -3,8 +3,6 @@ import { writeFileSync, existsSync, mkdirSync } from 'fs';
 import { join } from 'path';
 import { run } from 'shell-commands';
 
-import { apps, pages } from '../src/assets';
-
 const port = 3000;
 
 test.beforeAll(async () => {
@@ -12,13 +10,14 @@ test.beforeAll(async () => {
     rm -rf docs
     yarn parcel build src/index.html --dist-dir docs
   `);
-  run(`timeout 10s yarn http-server -p ${port} docs -c-1`); // auto quit after 5s
+  run(`timeout 5s yarn http-server -p ${port} docs -c-1`); // auto quit after 5s
 });
 test.afterAll(async () => {
   await run(`
     yarn lint
     cp -r saved/* docs/
     rm -rf saved
+    rm -rf test-results
   `);
 });
 
@@ -35,26 +34,19 @@ const saveFile = (path: string, _content: string) => {
   writeFileSync(join(folderPath, 'index.html'), content);
 };
 
-test('Home Page', async ({ page }) => {
+// 这是有点神奇的操作。把一个SPA转成传统的静态网站。
+// 原理很简单，就是把所有页面的内容都点出来，然后保存实时的html。
+test('Make the whole site static', async ({ page }) => {
   await page.goto(`http://localhost:${port}/`);
+  const content = await page.content();
   await expect(page).toHaveTitle(/MacMate.app - /);
-  saveFile('.', await page.content());
-});
-
-test('Apps', async ({ page }) => {
-  await page.goto(`http://localhost:${port}/`);
-  for (const app of apps) {
-    await page.getByTestId(app.marketing.path).click();
-    await expect(page).toHaveTitle(`${app.marketing.name} - MacMate.app`);
-    saveFile(app.marketing.path, await page.content());
-  }
-});
-
-test('Pages', async ({ page }) => {
-  await page.goto(`http://localhost:${port}/`);
-  for (const p of pages) {
-    await page.getByText(p.name).click();
-    await expect(page).toHaveTitle(`${p.name} - MacMate.app`);
-    saveFile(p.path, await page.content());
+  for (const href of content.match(/href=".+?"/g) ?? []) {
+    const path = href.substring(6, href.length - 1);
+    if (!(path.startsWith('/') && path.endsWith('/'))) {
+      continue;
+    }
+    const link = await page.$(`a[href="${path}"]`);
+    await link!.click();
+    saveFile(path, await page.content());
   }
 });
